f0fd07c74848872a07f2869a8b805d43eea93eb8
FBLudwig <freddy.ludwig@web.de>
2018-04-06 00:38:43 +0200

    Exercise 1b

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -5,7 +5,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello World \n");
+#pragma omp parallel num_threads(4)
+  printf("Hello World from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
 
   return 0;
 }
